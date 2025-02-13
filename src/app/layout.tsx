14d187d1e10d1ca318021749a4a905b2eb3e0184
EucTech <euccreative@gmail.com>
2025-02-13 06:17:48 -0500

    added updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/toaster"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -44,6 +45,7 @@ export default function RootLayout({
         }}
         >
         {children}
+        <Toaster />
         </ClerkProvider>
       </body>
     </html>
