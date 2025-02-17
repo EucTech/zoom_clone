7248ec10251fa975a358164e0b62e9bb15bd3305
EucTech <euccreative@gmail.com>
2025-02-17 04:17:53 -0500

    updated

--- src/components/MeetingRoom.tsx
+++ src/components/MeetingRoom.tsx
@@ -43,7 +43,7 @@ const MeetingRoom = () => {
           <CallLayout />
         </div>
         <div
-          className={cn("h-[calc(100vh-86px)] block ml-2", {
+          className={cn("h-[calc(100vh-86px)] hidden ml-2", {
             "show-block": showParticipants,
           })}
         >
@@ -58,20 +58,26 @@ const MeetingRoom = () => {
 
           <div className="flex items-center">
           <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b] ">
-            <LayoutList
+             <LayoutList
             size={20}
             className="text-white"
             />
           </DropdownMenuTrigger>
 
           </div>
-          <DropdownMenuContent>
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Profile</DropdownMenuItem>
-            <DropdownMenuItem>Billing</DropdownMenuItem>
-            <DropdownMenuItem>Team</DropdownMenuItem>
-            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          <DropdownMenuContent className="border-dark-1 bg-dark-1 text-white">
+            {['Grid', 'Speaker-Left', 'Speaker-Right'].map((item, index)=> (
+              <div key={index}>
+                <DropdownMenuItem className="cursor-pointer"
+                onClick={() => {setLayout(item.toLowerCase() as CallLayoutType)}}
+                >
+                  {item}
+                </DropdownMenuItem>
+            <DropdownMenuSeparator className="border-dark-1" />
+
+              </div>
+            ))}
+        
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
