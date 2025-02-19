96eae0b610b5d4d36b5f19b427108df46f30c0aa
EucTech <euccreative@gmail.com>
2025-02-19 11:51:08 -0500

    added function ro end call button

--- src/components/EndCallButton.tsx
+++ src/components/EndCallButton.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+"use client";
+import { useCall, useCallStateHooks } from "@stream-io/video-react-sdk";
+import React from "react";
 
 const EndCallButton = () => {
-  return (
-    <div>EndCallButton</div>
-  )
-}
+  const call = useCall();
 
-export default EndCallButton
+  const { useLocalParticipant } = useCallStateHooks();
+
+  const localParticipant = useLocalParticipant();
+
+  const isMeetingOwner =
+    localParticipant &&
+    call?.state.createdBy &&
+    localParticipant.userId === call.state.createdBy.id;
+
+    if (!isMeetingOwner) return null;
+
+  return (
+  <div>EndCallButton
+
+  </div>
+);
+};
+
+export default EndCallButton;
